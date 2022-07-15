25b3c935069710ed79970c3bec6785d68a1d3be4
Sniper7123 <83344260+Sniper7123@users.noreply.github.com>
2022-07-15 18:34:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,12 +29,12 @@ function my_keydown(e)
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
 		//write a code to check the type of key pressed
 		aplhabetkey();
 document.getElementById("dl").innerHTML="You pressed Alphabet key";
 console.log("alphabet key");
-
+		}
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
